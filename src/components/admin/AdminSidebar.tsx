b3c13024d24f65cc52435df21ffff0679ef9aac5
SatishMhobiya <70316467+SatishMhobiya@users.noreply.github.com>
2025-11-14 14:48:12 +0530

    added logo in admin sidebar

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -17,6 +17,7 @@ import {
 import { Link, Location, useLocation } from "react-router-dom";
 import { IconType } from "react-icons";
 import { MdDiscount } from "react-icons/md";
+import logo from "/logo.png";
 
 const AdminSidebar = () => {
   const location = useLocation();
@@ -60,7 +61,7 @@ const AdminSidebar = () => {
             : {}
         }
       >
-        <h2>Logo.</h2>
+        <img src={logo} alt="logo" width={48} height={48}/>
         <DivOne location={location} />
         <DivTwo location={location} />
         <DivThree location={location} />
